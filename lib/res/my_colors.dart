ca1a7b5b91b7bc131b1fd669807daaef932a7917
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-04-18 16:14:10 +0500

    all app colors all defiend in this file

---
+++ lib/res/my_colors.dart
@@ -0,0 +1,18 @@
+
+
+
+
+
+import 'package:flutter/material.dart';
+
+class MyColor {
+
+  static const Color backgroundColor = Color(0xFF09171f);
+  static const Color blackColor = Colors.black;
+  static const Color lightblackColor = Color(0xFF212d35);
+  static const Color grayColor = Colors.grey;
+  static const Color whiteColor = Colors.white;
+  static const Color greenAccentColor = Colors.greenAccent;
+  static const Color blueColor = Colors.blue;
+
+}
