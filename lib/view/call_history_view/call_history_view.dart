dad004cdfc4cab142e6ea5e25dfa842e15300963
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-04-18 16:16:08 +0500

    call history is almost ompleted

---
+++ lib/view/call_history_view/call_history_view.dart
@@ -0,0 +1,44 @@
+import 'package:chatter_box/res/widgets/my_text.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class CallHistoryView extends StatelessWidget {
+  const CallHistoryView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
+    final height = MediaQuery.of(context).size.height;
+    return SafeArea(
+      child: Scaffold(
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 12.0),
+          child: Column(
+            children: [
+              SizedBox(height: height * 0.02,),
+              MyText(title: 'Recent Calls',fontSize: 18,fontWeight: FontWeight.bold,),
+              SizedBox(height: height * 0.03,),
+
+              Expanded(
+                child: ListView.builder(
+                  itemCount: 12,
+                    itemBuilder: (context , index) {
+                      return Card(
+                        child: ListTile(
+                          contentPadding: EdgeInsets.symmetric(horizontal: 4),
+                          horizontalTitleGap: 12,
+                          leading: CircleAvatar(radius: 30,),
+                          title: MyText(title: 'Caller Name',fontSize: 18,fontWeight: FontWeight.w600,),
+                          subtitle: MyText(title: 'Yesterday, 8:47 PM'),
+                          trailing: IconButton(onPressed: (){},icon: Icon(Icons.call),),
+                        ),
+                      );
+                    }),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
