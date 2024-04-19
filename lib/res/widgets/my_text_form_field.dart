5c839e1459806fe7043b3433441706fd63a25940
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-04-19 23:08:47 +0500

    text form fied widget

---
+++ lib/res/widgets/my_text_form_field.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class MyTextFormField extends StatelessWidget {
+  const MyTextFormField({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      decoration: InputDecoration(
+
+      ),
+    );
+  }
+}
