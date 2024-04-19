1ca1dc4e77f7891e80960fbfb7bec372f3a5d263
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-04-19 23:54:05 +0500

    new colors is added

--- lib/res/my_colors.dart
+++ lib/res/my_colors.dart
@@ -15,4 +15,24 @@ class MyColor {
   static const Color greenAccentColor = Colors.greenAccent;
   static const Color blueColor = Colors.blue;
 
+  static List<String> images = [
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfntAvLz0v7HTH2tVKESiSyLMFQdaNadpbii-D7sv2SdpRhUqPab73dDbsKo_usrWy1n4&usqp=CAU',
+    'https://www.kindpng.com/picc/m/163-1636340_user-avatar-icon-avatar-transparent-user-icon-png.png',
+    'https://www.kindpng.com/picc/m/630-6306130_avatar-avatar-male-user-icon-hd-png-download.png',
+    'https://www.kindpng.com/picc/m/78-786207_user-avatar-png-user-avatar-icon-png-transparent.png',
+    'https://e7.pngegg.com/pngimages/348/800/png-clipart-man-wearing-blue-shirt-illustration-computer-icons-avatar-user-login-avatar-blue-child.png',
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSHuQG3EP3Sh3Iq2j7cgSkq1cmHcP3bmUO1CpVPtBfnHg&s',
+    'https://e7.pngegg.com/pngimages/348/800/png-clipart-man-wearing-blue-shirt-illustration-computer-icons-avatar-user-login-avatar-blue-child.png',
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfntAvLz0v7HTH2tVKESiSyLMFQdaNadpbii-D7sv2SdpRhUqPab73dDbsKo_usrWy1n4&usqp=CAU',
+    'https://www.kindpng.com/picc/m/163-1636340_user-avatar-icon-avatar-transparent-user-icon-png.png',
+    'https://www.kindpng.com/picc/m/630-6306130_avatar-avatar-male-user-icon-hd-png-download.png',
+    'https://www.kindpng.com/picc/m/78-786207_user-avatar-png-user-avatar-icon-png-transparent.png',
+    'https://i.pinimg.com/736x/a6/58/32/a65832155622ac173337874f02b218fb.jpg',
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSHuQG3EP3Sh3Iq2j7cgSkq1cmHcP3bmUO1CpVPtBfnHg&s',
+    'https://e7.pngegg.com/pngimages/348/800/png-clipart-man-wearing-blue-shirt-illustration-computer-icons-avatar-user-login-avatar-blue-child.png',
+    'https://www.kindpng.com/picc/m/163-1636340_user-avatar-icon-avatar-transparent-user-icon-png.png',
+    'https://www.kindpng.com/picc/m/630-6306130_avatar-avatar-male-user-icon-hd-png-download.png',
+    'https://www.kindpng.com/picc/m/78-786207_user-avatar-png-user-avatar-icon-png-transparent.png',
+  ];
+
 }
