f3f2fcb0f128eb8ee8d087d4b1ea69443ebf6366
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-04-21 23:23:53 +0500

    checking for user is loggedin or not

---
+++ lib/view_model/splash_view_model/splash_view_model.dart
@@ -0,0 +1,23 @@
+
+
+
+import 'dart:async';
+
+import 'package:chatter_box/view/auth_view/sign_in_view/sign_in_view.dart';
+import 'package:chatter_box/view/bottom_navigator_view/bottom_navigator_view.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class SplashViewModel {
+
+  final _auth = FirebaseAuth.instance.currentUser;
+
+  void SplashTime(BuildContext context)  {
+
+    Timer(Duration(seconds: 2) , () {
+      Navigator.push(context, MaterialPageRoute(builder: (context) => _auth != null ? BottomNavigatorView() : SignInView() ));
+    });
+  }
+
+}
