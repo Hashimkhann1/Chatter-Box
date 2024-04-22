836831fc73897db8ce3d382b6bc7054abac9ae5f
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-04-22 23:50:56 +0500

    ui is completed

--- lib/view/chat_view/chat_view.dart
+++ lib/view/chat_view/chat_view.dart
@@ -1,29 +1,38 @@
 import 'package:chatter_box/res/my_colors.dart';
 import 'package:chatter_box/res/widgets/my_text.dart';
+import 'package:chatter_box/view_model/chat_view_model/chat_view_model.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 class ChatView extends StatelessWidget {
-  const ChatView({super.key});
+  final String userName;
+  final String reciverId;
+  ChatView({super.key,required this.userName,required this.reciverId});
+
+  final _auth = FirebaseAuth.instance.currentUser!;
+
+  final _messageController = TextEditingController();
+  // final ChatViewModel chatViewModel = ChatViewModel();
 
   @override
   Widget build(BuildContext context) {
-    final width = MediaQuery.of(context).size.width;
-    final height = MediaQuery.of(context).size.height;
+    // final width = MediaQuery.of(context).size.width;
+    // final height = MediaQuery.of(context).size.height;
 
     return Scaffold(
       backgroundColor: MyColor.backgroundColor,
       appBar: AppBar(
         title: MyText(
-          title: 'User Name',
+          title: userName.toString(),
           fontSize: 18,
           fontWeight: FontWeight.w700,
         ),
         actions: [
           IconButton(
               onPressed: () {},
-              icon: Icon(
+              icon: const Icon(
                 CupertinoIcons.phone_fill,
                 color: MyColor.grayColor,
               ))
@@ -35,73 +44,109 @@ class ChatView extends StatelessWidget {
         padding: const EdgeInsets.symmetric(horizontal: 13.0, vertical: 6),
         child: Column(
           children: [
-            /////// messages ///////
+            StreamBuilder(
+                stream: FirebaseFirestore.instance.collection('chat_rooms').doc('${_auth.uid}_$reciverId').collection('messages').snapshots(),
+                builder: (context , snapshot) {
+                  if(snapshot.hasError){
+                    return const Center(
+                      child: CircularProgressIndicator(color: MyColor.whiteColor,),
+                    );
+                  }
+                  else if(snapshot.connectionState == ConnectionState.waiting){
+                    return const Center(
+                      child: CircularProgressIndicator(color: MyColor.whiteColor,),
+                    );
+                  }
+                  print(_auth.uid+'_'+reciverId);
+                  print(snapshot.data!.docs.length);
+                  return Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.only(bottom: 16.0, top: 10),
+                      child: snapshot.data!.docs.length == 0 ? const Center(
+                        child: MyText(title: 'Message not recieve or sended yet!',fontSize: 17,fontWeight: FontWeight.w700,),
+                      ) :  ListView.builder(
+                          itemCount: snapshot.data!.docs.length,
+                          itemBuilder: (context, index) {
 
-            Expanded(
-              child: Padding(
-                padding: const EdgeInsets.only(bottom: 16.0, top: 10),
-                child: ListView.builder(
-                    itemCount: 10,
-                    itemBuilder: (context, index) {
-                      return Align(
-                        alignment: index % 2 == 1
-                            ? Alignment.bottomRight
-                            : Alignment.centerLeft,
-                        child: Container(
-                          width: width * 0.70,
-                          margin: EdgeInsets.symmetric(vertical: 5),
-                          padding: EdgeInsets.all(10),
-                          decoration: BoxDecoration(
-                              color: index % 2 == 1 ? MyColor.blueColor : Colors.grey.shade800,
-                              borderRadius: BorderRadius.only(
-                                bottomLeft: Radius.circular(20),
-                                bottomRight: Radius.circular(index % 2 == 1 ? 0 : 20),
-                                topLeft: Radius.circular(index % 2 == 0 ? 0 : 20),
-                                topRight: Radius.circular(20)
-                              )),
-                          child: MyText(
-                            title:
-                                'This is the user message from both sender and reciver and ohter thing this is what saoainkb',
-                            fontSize: 16,
-                          ),
-                        ),
-                      );
-                    }),
-              ),
-            ),
+                            dynamic data = snapshot.data!.docs[index];
+
+                            return Align(
+                              alignment: _auth.uid.toString() == data['senderUid']
+                                  ? Alignment.bottomRight
+                                  : Alignment.centerLeft,
+                              child: Container(
+                                width: 200,
+                                margin: const EdgeInsets.symmetric(vertical: 5),
+                                padding: const EdgeInsets.all(10),
+                                decoration: BoxDecoration(
+                                    color: snapshot.data!.docs[index]['senderUid'] != reciverId ? Colors.grey.shade800 : MyColor.blueColor,
+                                    borderRadius: BorderRadius.only(
+                                        bottomLeft: const Radius.circular(24),
+                                        bottomRight: Radius.circular(_auth.uid.toString() == data['senderUid'] ? 0 : 24),
+                                        topLeft: Radius.circular(_auth.uid.toString() != data['senderUid'] ? 0 : 22),
+                                        topRight: const Radius.circular(24)
+                                    )),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    MyText(
+                                      title:
+                                      snapshot.data!.docs[index]['message'].toString(),
+                                      fontSize: 16,
+                                    ),
+                                    // SizedBox(height: height * 0.001,),
+                                    const Align(
+                                      alignment: Alignment.centerRight,
+                                        child: MyText(title: '9:56 AM',fontSize: 12,))
+                                  ],
+                                ),
+                              ),
+                            );
+                          }),
+                    ),
+                  );
+                }),
 
             //////// message text field and send button//////
             Container(
               alignment: Alignment.center,
-              width: width,
-              height: height * 0.07,
+              // width: width,
+              // height: height * 0.07,
               decoration: BoxDecoration(
-                color: MyColor.lightblackColor,
-                borderRadius: BorderRadius.circular(30)
+                  color: MyColor.lightblackColor,
+                  borderRadius: BorderRadius.circular(30)
               ),
               child: Row(
                 children: [
                   Expanded(
-                    child: TextField(
+                    child: TextFormField(
                       decoration: InputDecoration(
-                          hintText: "Type something...",
-                          fillColor: MyColor.lightblackColor,
+                          prefixIcon: const Icon(
+                            CupertinoIcons.search,
+                            color: MyColor.grayColor,
+                          ),
+                          hintText: "Search you chat",
+                          hintStyle:
+                          const TextStyle(color: MyColor.grayColor, fontSize: 13),
                           filled: true,
+                          fillColor: MyColor.lightblackColor,
                           border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(30.0),
-                            borderSide: BorderSide.none
-                          )
-                      ),
+                              borderRadius: BorderRadius.circular(8),
+                              borderSide: BorderSide.none)),
                     ),
                   ),
                   IconButton(
-                      onPressed: (){},
-                      icon: Icon(Icons.send)
+                      onPressed: (){
+                        // if(_messageController.text.isNotEmpty){
+                        //   chatViewModel.sendMessage(context, reciverId, _messageController.text.toString());
+                        //   _messageController.clear();
+                        // }
+                      },
+                      icon: const Icon(Icons.send)
                   )
                 ],
               ),
             ),
-
           ],
         ),
       ),
