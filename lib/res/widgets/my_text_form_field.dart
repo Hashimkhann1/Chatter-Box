c9206f6c2ab2725f1ebc5ce3164cb4090affbc06
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-04-22 23:48:27 +0500

    text form field widget

--- lib/res/widgets/my_text_form_field.dart
+++ lib/res/widgets/my_text_form_field.dart
@@ -1,14 +1,47 @@
+import 'package:chatter_box/res/my_colors.dart';
 import 'package:flutter/material.dart';
 
 class MyTextFormField extends StatelessWidget {
-  const MyTextFormField({super.key});
+  final String? hintText;
+  final Color? hintTextColor;
+  final double? fontSize;
+  final Widget? prefixIcon;
+  final bool obscureText;
+  final InputBorder? border;
+  final BorderSide enableBorderSide;
+  final BorderSide focusedBorderSide;
+  final String? Function(String?)? validator;
+  final TextEditingController? controller;
+
+  const MyTextFormField(
+      {super.key,
+      required this.hintText,
+      this.hintTextColor,
+      this.fontSize,
+      this.prefixIcon,
+      this.obscureText = false,
+      this.border,
+      this.enableBorderSide =
+          const BorderSide(color: MyColor.blueColor, width: 2),
+      this.focusedBorderSide =
+          const BorderSide(color: MyColor.blueColor, width: 2),
+      this.validator,
+      required this.controller});
 
   @override
   Widget build(BuildContext context) {
     return TextFormField(
+      obscureText: obscureText,
+      controller: controller,
       decoration: InputDecoration(
-
+        prefixIcon: prefixIcon,
+        hintText: hintText,
+        hintStyle: TextStyle(fontSize: fontSize, color: hintTextColor),
+        border: border,
+        enabledBorder: UnderlineInputBorder(borderSide: enableBorderSide),
+        focusedBorder: UnderlineInputBorder(borderSide: enableBorderSide),
       ),
+      validator: validator,
     );
   }
 }
