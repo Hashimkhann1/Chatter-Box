92152a89a3f3e25f7dd7f7ca59263c5caa7823a5
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-04-23 23:33:38 +0500

    text chat is completed

--- lib/view/home_view/home_view.dart
+++ lib/view/home_view/home_view.dart
@@ -1,12 +1,16 @@
 import 'package:chatter_box/res/my_colors.dart';
 import 'package:chatter_box/view/home_view/all_users_view/all_users_view.dart';
 import 'package:chatter_box/view/home_view/recent_update_view/recent_update_view.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 class HomeView extends StatelessWidget {
-  const HomeView({super.key});
+  HomeView({super.key});
+
+  final _auth = FirebaseAuth.instance.currentUser!;
 
   @override
   Widget build(BuildContext context) {
@@ -20,7 +24,9 @@ class HomeView extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            SizedBox(height: height * 0.05,),
+            SizedBox(
+              height: height * 0.05,
+            ),
 
             //////// Top text field ////////
             Container(
@@ -28,45 +34,70 @@ class HomeView extends StatelessWidget {
               height: height * 0.06,
               child: TextFormField(
                 decoration: InputDecoration(
-                  prefixIcon: Icon(CupertinoIcons.search,color: MyColor.grayColor,),
-                  hintText: "Search you chat",
-                  hintStyle: TextStyle(
-                    color: MyColor.grayColor,
-                    fontSize: 13
-                  ),
-                  filled: true,
-                  fillColor: MyColor.lightblackColor,
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(8),
-                    borderSide: BorderSide.none
-                  )
-                ),
+                    prefixIcon: Icon(
+                      CupertinoIcons.search,
+                      color: MyColor.grayColor,
+                    ),
+                    hintText: "Search you chat",
+                    hintStyle:
+                        TextStyle(color: MyColor.grayColor, fontSize: 13),
+                    filled: true,
+                    fillColor: MyColor.lightblackColor,
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8),
+                        borderSide: BorderSide.none)),
               ),
             ),
 
             //////// Recent Update Widget ///////
             RecentUpdateView(),
-            SizedBox(height: height * 0.01,),
+            SizedBox(
+              height: height * 0.01,
+            ),
 
             ////// divider /////
-            Divider(color: Colors.grey.shade800,),
-            SizedBox(height: height * 0.01,),
+            Divider(
+              color: Colors.grey.shade800,
+            ),
+            SizedBox(
+              height: height * 0.01,
+            ),
 
             /////// All users ///////
-            Expanded(
-              child: ListView.builder(
-                itemCount: MyColor.images.length,
-                  itemBuilder: (context , index) {
-                  return Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 4.0),
-                    child: AllUsersView(imagesPath: MyColor.images[index].toString(),index: index,),
+            StreamBuilder(
+                stream:
+                    FirebaseFirestore.instance.collection('users').snapshots(),
+                builder: (context, snapshot) {
+                  if (snapshot.hasError) {
+                    return Center(
+                      child: CircularProgressIndicator(
+                        color: Colors.red,
+                      ),
+                    );
+                  } else if (snapshot.connectionState ==
+                      ConnectionState.waiting) {
+                    return Center(
+                      child: CircularProgressIndicator(
+                        color: MyColor.whiteColor,
+                      ),
+                    );
+                  }
+                  return Expanded(
+                    child: ListView.builder(
+                        itemCount: snapshot.data!.docs.length,
+                        itemBuilder: (context, index) {
+                          return Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 4.0),
+                            child: snapshot.data!.docs[index].id.toString() == _auth.uid.toString() ? SizedBox() : AllUsersView(
+                              userName: snapshot.data!.docs[index]['userName']
+                                  .toString(),
+                              imagesPath: MyColor.images[index].toString(),
+                              reciverId: snapshot.data!.docs[index].id.toString(),
+                            ),
+                          );
+                        }),
                   );
-                  }),
-            )
-
-
-
-
+                })
           ],
         ),
       ),
