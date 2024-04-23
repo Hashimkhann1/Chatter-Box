334da143c48a516cd9d66ba04959f1afeeeaff0e
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-04-23 23:35:01 +0500

    method is added for getting chats

--- lib/view_model/chat_view_model/chat_view_model.dart
+++ lib/view_model/chat_view_model/chat_view_model.dart
@@ -7,29 +7,79 @@ import 'package:flutter/cupertino.dart';
 
 class ChatViewModel {
 
-  final _firestore = FirebaseFirestore.instance.collection('chat_rooms');
+  final _firestore = FirebaseFirestore.instance;
   final _auth = FirebaseAuth.instance;
   final Constant constant = Constant();
 
   /// send message method
-  void sendMessage(BuildContext context , String reciverId , message) async {
+  // Future sendMessage(BuildContext context , String reciverId , message) async {
+  //
+  //   try{
+  //
+  //     final String senderUid = _auth.currentUser!.uid.toString();
+  //     final String senderEmail = _auth.currentUser!.email.toString();
+  //     final Timestamp timestamp = Timestamp.now();
+  //
+  //     print(senderUid);
+  //     print(reciverId);
+  //
+  //     await _firestore.doc('${senderUid}_$reciverId').collection('messages').add({
+  //       'senderUid': senderUid,
+  //       'senderEmail': senderEmail,
+  //       'reciverId': reciverId,
+  //       'message':message,
+  //       'timestamp':timestamp
+  //     });
+  //   }catch(error){
+  //     constant.toastMessage('Unable to send message try again');
+  //   }
+  // }
 
-    try{
 
-      final String senderUid = _auth.currentUser!.uid.toString();
-      final String senderEmail = _auth.currentUser!.email.toString();
+// send message
+  Future<void> sendMessage(String reciverID, message) async {
+    try {
+      // get currrent user info
+      final String currentUserId = _auth.currentUser!.uid.toString();
+      final String currentUserEmail = _auth.currentUser!.email.toString();
       final Timestamp timestamp = Timestamp.now();
 
-      await _firestore.doc('${senderUid}_$reciverId').collection('messages').add({
-        'senderUid': senderUid,
-        'senderEmail': senderEmail,
-        'reciverId': reciverId,
+      // construct chat room ID for the two users (sorted to ensure)
+      List<String> ids = [currentUserId, reciverID];
+      ids.sort();
+      String chatRoomId = ids.join('_');
+
+      // add new message to database
+      await _firestore
+          .collection('chat_rooms')
+          .doc(chatRoomId)
+          .collection('messages')
+          .add({
+        'senderUid': currentUserId,
+        'senderEmail': currentUserEmail,
+        'reciverId': reciverID,
         'message':message,
         'timestamp':timestamp
       });
-    }catch(error){
-      constant.toastMessage('Unable to send message try again');
+    } catch (error) {
+      print(">>>>> error from adding new message");
     }
+  }
+
+
+  //get message
+  Stream<QuerySnapshot> getMessages(String userId, otherUserId) {
+    // construct a chatroom ID for the two users
+    List<String> ids = [userId, otherUserId];
+    ids.sort();
+    String chatRoomID = ids.join('_');
+
+    return _firestore
+        .collection('chat_rooms')
+        .doc(chatRoomID)
+        .collection('messages')
+        .orderBy('timestamp', descending: false)
+        .snapshots();
   }
 
 }
