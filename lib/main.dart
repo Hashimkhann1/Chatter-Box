1b28aaed75e8d22ef28159e6ae16b28b1a92bb2c
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-04-30 22:58:18 +0500

    pick image bloc is added

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:chatter_box/firebase_options.dart';
 import 'package:chatter_box/view/splash_view/splash_view.dart';
 import 'package:chatter_box/view_model/bloc/loading_bloc/loading_bloc/loading_bloc.dart';
+import 'package:chatter_box/view_model/bloc/pick_image_from_gallery/pick_image_bloc/pick_image_bloc.dart';
+import 'package:chatter_box/view_model/chat_view_model/chat_view_model.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -21,7 +23,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiBlocProvider(
         providers: [
-          BlocProvider(create: (_) => LoadingBloc())
+          BlocProvider(create: (_) => LoadingBloc()),
+          BlocProvider(create: (_) => PickImageBloc(ChatViewModel()))
         ],
         child: MaterialApp(
         title: 'Flutter Demo',
