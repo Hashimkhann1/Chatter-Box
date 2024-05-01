8c2e52c181c00ed3c640f773b48b69e62d5b9933
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-05-01 22:42:39 +0500

    pick image is completed

--- lib/view_model/bloc/pick_image_from_gallery/pick_image_bloc/pick_image_bloc.dart
+++ lib/view_model/bloc/pick_image_from_gallery/pick_image_bloc/pick_image_bloc.dart
@@ -1,9 +1,12 @@
 
 
 import 'package:bloc/bloc.dart';
+import 'package:chatter_box/view/chat_view/shoe_detail_image_view/shoe_detail_image_view.dart';
 import 'package:chatter_box/view_model/bloc/pick_image_from_gallery/pick_image_event_bloc/pick_image_event_bloc.dart';
 import 'package:chatter_box/view_model/bloc/pick_image_from_gallery/pick_image_state/pick_image_state.dart';
 import 'package:chatter_box/view_model/chat_view_model/chat_view_model.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:image_picker/image_picker.dart';
 
 class PickImageBloc extends Bloc<PickImageEventBloc , PickImageState> {
@@ -17,6 +20,7 @@ class PickImageBloc extends Bloc<PickImageEventBloc , PickImageState> {
 
   void _pickImageFromGallery(PickImageFromGallery event , Emitter emit) async {
     XFile? file = await chatViewModel.pickImageFromGllery();
+    // Navigator.push(context, MaterialPageRoute(builder: (context) => ShowDetailImageView()));
     emit(state.copyWith(file: file));
   }
 
