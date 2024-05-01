a5150fcfe623a612084d79658cdadd662be2bfa3
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-05-01 22:39:04 +0500

    show chat image in detail

---
+++ lib/view/chat_view/shoe_detail_image_view/shoe_detail_image_view.dart
@@ -0,0 +1,12 @@
+
+
+import 'package:flutter/material.dart';
+
+class ShowDetailImageView extends StatelessWidget {
+  const ShowDetailImageView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
