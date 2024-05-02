f0b33e0450203698bfccc593b78f17213a76947f
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-05-02 23:12:47 +0500

    pick image from gallery method is added

--- lib/view_model/bloc/pick_image_from_gallery/pick_image_bloc/pick_image_bloc.dart
+++ lib/view_model/bloc/pick_image_from_gallery/pick_image_bloc/pick_image_bloc.dart
@@ -1,12 +1,13 @@
 
 
+import 'dart:io';
+
 import 'package:bloc/bloc.dart';
-import 'package:chatter_box/view/chat_view/shoe_detail_image_view/shoe_detail_image_view.dart';
+import 'package:chatter_box/view/chat_view/show_detail_image_view/show_detail_image_view.dart';
 import 'package:chatter_box/view_model/bloc/pick_image_from_gallery/pick_image_event_bloc/pick_image_event_bloc.dart';
 import 'package:chatter_box/view_model/bloc/pick_image_from_gallery/pick_image_state/pick_image_state.dart';
 import 'package:chatter_box/view_model/chat_view_model/chat_view_model.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:image_picker/image_picker.dart';
 
 class PickImageBloc extends Bloc<PickImageEventBloc , PickImageState> {
@@ -19,8 +20,10 @@ class PickImageBloc extends Bloc<PickImageEventBloc , PickImageState> {
   }
 
   void _pickImageFromGallery(PickImageFromGallery event , Emitter emit) async {
-    XFile? file = await chatViewModel.pickImageFromGllery();
-    // Navigator.push(context, MaterialPageRoute(builder: (context) => ShowDetailImageView()));
+    XFile? file = await chatViewModel.pickImageFromGallery();
+    if(file != null){
+      Navigator.push(event.context, MaterialPageRoute(builder: (context) => ShowDetailImageView(imagePath: file.path,reciverId: event.reciverId,)));
+    }
     emit(state.copyWith(file: file));
   }
 
