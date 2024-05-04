1f9da4281427d3478920a93856e528e967e3a29d
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-05-04 23:29:59 +0500

    image and text chat is seperated by message type and assign them to its widget

--- lib/view/chat_view/chat_view.dart
+++ lib/view/chat_view/chat_view.dart
@@ -2,6 +2,8 @@
 
 import 'package:chatter_box/res/my_colors.dart';
 import 'package:chatter_box/res/widgets/my_text.dart';
+import 'package:chatter_box/view/chat_view/chat_type_widgets/image_chat_widget/image_chat_widget.dart';
+import 'package:chatter_box/view/chat_view/chat_type_widgets/text_chat_widget/text_chat_widget.dart';
 import 'package:chatter_box/view_model/bloc/pick_image_from_gallery/pick_image_bloc/pick_image_bloc.dart';
 import 'package:chatter_box/view_model/bloc/pick_image_from_gallery/pick_image_event_bloc/pick_image_event_bloc.dart';
 import 'package:chatter_box/view_model/chat_view_model/chat_view_model.dart';
@@ -79,33 +81,7 @@ class ChatView extends StatelessWidget {
                               alignment: _auth.uid.toString() == data['senderUid']
                                   ? Alignment.bottomRight
                                   : Alignment.centerLeft,
-                              child: Container(
-                                width: 200,
-                                margin: const EdgeInsets.symmetric(vertical: 5),
-                                padding: const EdgeInsets.symmetric(horizontal: 8,vertical: 4),
-                                decoration: BoxDecoration(
-                                    color: snapshot.data!.docs[index]['senderUid'] != reciverId ? Colors.grey.shade800 : MyColor.blueColor,
-                                    borderRadius: BorderRadius.only(
-                                        bottomLeft: const Radius.circular(24),
-                                        bottomRight: Radius.circular(_auth.uid.toString() == data['senderUid'] ? 0 : 24),
-                                        topLeft: Radius.circular(_auth.uid.toString() != data['senderUid'] ? 0 : 22),
-                                        topRight: const Radius.circular(24)
-                                    )),
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    MyText(
-                                      title:
-                                      snapshot.data!.docs[index]['message'].toString(),
-                                      fontSize: 16,
-                                    ),
-                                    // SizedBox(height: height * 0.001,),
-                                    const Align(
-                                      alignment: Alignment.centerRight,
-                                        child: MyText(title: '9:56 AM',fontSize: 12,))
-                                  ],
-                                ),
-                              ),
+                              child: data['messageType'] == 'text' ? TextChatWidget(chatData: data,reciverId: reciverId,) : ImageChatWidget(chatData: data, reciverId: reciverId)
                             );
                           }),
                     ),
