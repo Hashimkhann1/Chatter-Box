f54dc2a8b52a0d48200d8871a7a0374f957e558b
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-05-04 23:28:22 +0500

    image chat widget is completed

---
+++ lib/view/chat_view/chat_type_widgets/image_chat_widget/image_chat_widget.dart
@@ -0,0 +1,62 @@
+import 'package:chatter_box/res/my_colors.dart';
+import 'package:chatter_box/res/widgets/my_text.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class ImageChatWidget extends StatelessWidget {
+  final dynamic chatData;
+  final String reciverId;
+
+  ImageChatWidget({super.key,required this.chatData,required this.reciverId});
+
+  final _auth = FirebaseAuth.instance.currentUser!;
+
+  @override
+  Widget build(BuildContext context) {
+    final height = MediaQuery.sizeOf(context).height;
+
+    return Container(
+      width: 200,
+      margin: const EdgeInsets.symmetric(vertical: 5),
+      padding: EdgeInsets.all(4),
+      decoration: BoxDecoration(
+          color: chatData['senderUid'] != reciverId ? Colors.grey.shade800 : MyColor.blueColor,
+          borderRadius: BorderRadius.only(
+              bottomLeft: const Radius.circular(24),
+              bottomRight: Radius.circular(_auth.uid.toString() == chatData['senderUid'] ? 0 : 24),
+              topLeft: Radius.circular(_auth.uid.toString() != chatData['senderUid'] ? 0 : 22),
+              topRight: const Radius.circular(24)
+          )),
+      child: Stack(
+        // crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            height: height * 0.27,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: NetworkImage(chatData['imageUrl'].toString()),
+                fit: BoxFit.cover
+              ),
+              borderRadius: BorderRadius.only(
+                  bottomLeft: const Radius.circular(24),
+                  bottomRight: Radius.circular(_auth.uid.toString() == chatData['senderUid'] ? 0 : 24),
+                  topLeft: Radius.circular(_auth.uid.toString() != chatData['senderUid'] ? 0 : 22),
+                  topRight: const Radius.circular(24)
+              )
+            ),
+          ),
+
+          // SizedBox(height: height * 0.001,),
+          Positioned(
+            bottom: 3,
+            right: 14,
+            child: const Align(
+                alignment: Alignment.centerRight,
+                child: MyText(title: '9:56 AM',fontSize: 11,fontWeight: FontWeight.bold,)),
+          )
+        ],
+      ),
+    );
+  }
+}
