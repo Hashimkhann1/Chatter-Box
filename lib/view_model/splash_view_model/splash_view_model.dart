ded6e78af4bafbc7f7fee006fbfc874475ead85e
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-06-01 23:38:05 +0500

    updated for calling current user data method

--- lib/view_model/splash_view_model/splash_view_model.dart
+++ lib/view_model/splash_view_model/splash_view_model.dart
@@ -5,8 +5,11 @@ import 'dart:async';
 
 import 'package:chatter_box/view/auth_view/sign_in_view/sign_in_view.dart';
 import 'package:chatter_box/view/bottom_navigator_view/bottom_navigator_view.dart';
+import 'package:chatter_box/view_model/bloc/current_user_bloc/current_user_bloc/current_user_bloc.dart';
+import 'package:chatter_box/view_model/bloc/current_user_bloc/current_user_event/current_user_event.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class SplashViewModel {
 
@@ -15,6 +18,7 @@ class SplashViewModel {
   void SplashTime(BuildContext context)  {
 
     Timer(const Duration(seconds: 2) , () {
+      context.read<CurrentUserBloc>().add(CurrentUserData());
       Navigator.push(context, MaterialPageRoute(builder: (context) => _auth != null ? const BottomNavigatorView() : SignInView() ));
     });
   }
