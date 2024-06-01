877e277e28d6cd754e2e4130322c4f286e4d9f12
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-06-01 23:35:46 +0500

    current user event

---
+++ lib/view_model/bloc/current_user_bloc/current_user_event/current_user_event.dart
@@ -0,0 +1,13 @@
+
+
+
+import 'package:equatable/equatable.dart';
+
+abstract class CurrentUserEvent extends Equatable{
+
+  @override
+  List<Object> get props => [];
+
+}
+
+class CurrentUserData extends CurrentUserEvent {}
