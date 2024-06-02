fd5f22e204698679304413cdd6579c5a0e16298b
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-06-02 22:43:50 +0500

    updated for voice message and working o it

--- lib/view/chat_view/chat_view.dart
+++ lib/view/chat_view/chat_view.dart
@@ -1,5 +1,8 @@
 // import 'dart:js';
 
+import 'dart:io';
+
+import 'package:audioplayers/audioplayers.dart';
 import 'package:chatter_box/res/my_colors.dart';
 import 'package:chatter_box/res/widgets/my_text.dart';
 import 'package:chatter_box/view/chat_view/chat_type_widgets/image_chat_widget/image_chat_widget.dart';
@@ -11,27 +14,81 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_sound/flutter_sound.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:permission_handler/permission_handler.dart';
 
-class ChatView extends StatelessWidget {
+
+class ChatView extends StatefulWidget {
   final String userName;
   final String reciverId;
   ChatView({super.key,required this.userName,required this.reciverId});
 
-  final _auth = FirebaseAuth.instance.currentUser!;
+  @override
+  State<ChatView> createState() => _ChatViewState();
+}
 
+class _ChatViewState extends State<ChatView> {
+
+
+  final _auth = FirebaseAuth.instance.currentUser!;
   final _messageController = TextEditingController();
   final ChatViewModel chatViewModel = ChatViewModel();
+
+  /// sound recoder and player
+
+  @override
+  void initState() {
+    initRecorder();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    recorder.closeRecorder();
+    super.dispose();
+  }
+
+  final recorder = FlutterSoundRecorder();
+
+  Future initRecorder() async {
+    final status = await Permission.microphone.request();
+    if (status != PermissionStatus.granted) {
+      throw 'Permission not granted';
+    }
+    await recorder.openRecorder();
+    recorder.setSubscriptionDuration(const Duration(milliseconds: 500));
+  }
+
+  Future startRecord() async {
+    await recorder.startRecorder(toFile: "audio");
+  }
+
+  Future stopRecorder() async {
+    final filePath = await recorder.stopRecorder();
+    if(filePath != null){
+    final file = File(filePath);
+    print('Recorded file path: $filePath');
+    print(file);
+    final player = await AudioPlayer();
+    player.play();
+    // player.play(file as Source);
+    }
+  }
+
 
   @override
   Widget build(BuildContext context) {
     // final width = MediaQuery.of(context).size.width;
     // final height = MediaQuery.of(context).size.height;
 
+
+
     return Scaffold(
       backgroundColor: MyColor.backgroundColor,
       appBar: AppBar(
         title: MyText(
-          title: userName.toString(),
+          title: widget.userName.toString(),
           fontSize: 18,
           fontWeight: FontWeight.w700,
         ),
@@ -51,7 +108,7 @@ class ChatView extends StatelessWidget {
         child: Column(
           children: [
             StreamBuilder(
-                stream: chatViewModel.getMessages(_auth.uid.toString(), reciverId.toString()),
+                stream: chatViewModel.getMessages(_auth.uid.toString(), widget.reciverId.toString()),
                 builder: (context , snapshot) {
                   if(snapshot.hasError){
                     return const Center(
@@ -63,7 +120,7 @@ class ChatView extends StatelessWidget {
                       child: CircularProgressIndicator(color: MyColor.whiteColor,),
                     );
                   }
-                  print('${_auth.uid}_$reciverId');
+                  print('${_auth.uid}_${widget.reciverId}');
                   print(snapshot.data!.docs.length);
                   return Expanded(
                     child: Padding(
@@ -80,7 +137,7 @@ class ChatView extends StatelessWidget {
                               alignment: _auth.uid.toString() == data['senderUid']
                                   ? Alignment.bottomRight
                                   : Alignment.centerLeft,
-                              child: data['messageType'] == 'text' ? TextChatWidget(chatData: data,reciverId: reciverId,) : ImageChatWidget(chatData: data, reciverId: reciverId)
+                              child: data['messageType'] == 'text' ? TextChatWidget(chatData: data,reciverId: widget.reciverId,) : ImageChatWidget(chatData: data, reciverId: widget.reciverId)
                             );
                           }),
                     ),
@@ -107,7 +164,21 @@ class ChatView extends StatelessWidget {
                           //   color: MyColor.grayColor,
                           // ),
                           hintText: "Type something...",
-                          prefixIcon: const Icon(Icons.mic),
+                          prefixIcon: GestureDetector(
+                            onTap: () async {
+                              if (recorder.isRecording) {
+                                await stopRecorder();
+                                setState(() {});
+                              } else {
+                                await startRecord();
+                                setState(() {});
+                              }
+                            },
+                            child: Icon(
+                              recorder.isRecording ? Icons.stop : Icons.mic,
+                              color: recorder.isRecording ? Colors.red : null,
+                            ),
+                          ),
                           hintStyle:
                           const TextStyle(color: MyColor.grayColor, fontSize: 14),
                           filled: true,
@@ -119,7 +190,7 @@ class ChatView extends StatelessWidget {
                   ),
                   IconButton(
                       onPressed: () async {
-                        context.read<PickImageBloc>().add(PickImageFromGallery(context: context,reciverId: reciverId));
+                        context.read<PickImageBloc>().add(PickImageFromGallery(context: context,reciverId: widget.reciverId));
                         // if(_messageController.text.isNotEmpty){
                         //    await chatViewModel.sendMessage(reciverId, _messageController.text.toString());
                         //   _messageController.clear();
@@ -130,7 +201,7 @@ class ChatView extends StatelessWidget {
                   IconButton(
                       onPressed: () async {
                         if(_messageController.text.isNotEmpty){
-                           await chatViewModel.sendMessage(reciverId, _messageController.text.toString());
+                           await chatViewModel.sendMessage(widget.reciverId, _messageController.text.toString());
                           _messageController.clear();
                         }
                       },
